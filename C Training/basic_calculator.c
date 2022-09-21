b88a5332d5e19023568e8bfaf057f8c172d37ca2
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2022-09-21 13:01:54 +0530

    C Training updated

--- C Training/basic_calculator.c
+++ C Training/basic_calculator.c
@@ -2,7 +2,13 @@
 #include<stdlib.h>
 
 int main(){
-    printf("Basic Calculator..\n");
+    double first,second;
+    printf("Enter first number: ");
+    scanf("%lf",&first);
+    printf("Enter second number: ");
+    scanf("%lf",&second);
+    double sum=first+second;
+    printf("Sum of %0.2f and %0.2f is: %0.2f \n",first,second,sum);
 
     return 0;
 }
